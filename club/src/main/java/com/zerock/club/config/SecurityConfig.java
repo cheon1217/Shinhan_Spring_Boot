47c8f04ea68e020db8e13334818ed43783bbe8df
wocjs <friendly1217@naver.com>
2025-08-05 17:48:03 +0900

    spring security study start

---
+++ club/src/main/java/com/zerock/club/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.zerock.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig {
+}
